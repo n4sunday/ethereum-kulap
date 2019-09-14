26ff1e53db312a816907ea7db905741bff8b4fcb
n4s <s5935512004@phuket.psu.ac.th>
2019-09-14 13:21:37 +0700

    first commit

---
+++ test/Inbox.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+const web3 = new Web3(ganache.provider())
+
+let accounts
+
+beforeEach(async () => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts()
+
+    // Use one of those accoints to deploy
+    // the contracts
+})
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(accounts)
+    })
+})
